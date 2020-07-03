d1ed7e3f58d095c5e4ecf9ab08a1858cf19f010c
Allison Mackey <a.mackey693@gmail.com>
2020-07-02 21:09:35 -0700

    create initial Header.js and import to App.js file, fix bug with index.js file to import App.js correctly

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <React.Fragment>
-      
+      <Header/>
     </React.Fragment>
   );
 }
