cb10e87542d7b70a6c252d5a5da168351e7a43ad
Allison Mackey <a.mackey693@gmail.com>
2020-07-02 21:11:17 -0700

    add LandingPage.js

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LandingPage(){
+  return(
+    <React.Fragment>
+      <h1>Welcome To Our TapRoom!</h1>
+      <p>Info will go here!</p>
+    </React.Fragment>
+  );
+}
+
+export default Header;
